b02dc68109aeaa2468f422abf7ba22132d14c47d
Script <bigalex934@gmail.com>
2024-02-14 12:07:01 +0300

    Implement Poco lib sample

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,28 @@
+#include <Poco/Net/DNS.h>
+#include <Poco/Net/HostEntry.h>
+#include <Poco/Net/IPAddress.h>
 #include <iostream>
 
+using Poco::Net::DNS;
+using Poco::Net::HostEntry;
+using Poco::Net::IPAddress;
+
 int32_t main(int32_t argc, char *argv[]) {
-  std::cout << "Hello world" << std::endl;
-  return 0;
+    const HostEntry &entry = DNS::hostByName("www.google.com");
+    std::cout << "Canonical name: " << entry.name() << std::endl;
+
+    const HostEntry::AliasList &aliases = entry.aliases();
+    HostEntry::AliasList::const_iterator alias_it = aliases.begin();
+
+    for (; alias_it != aliases.end(); ++alias_it) {
+        std::cout << "Alias: " << *alias_it << std::endl;
+    }
+
+    const HostEntry::AddressList &addrs = entry.addresses();
+    HostEntry::AddressList::const_iterator addr_it = addrs.begin();
+    for (; addr_it != addrs.end(); ++addr_it) {
+        std::cout << "Address: " << addr_it->toString() << std::endl;
+    }
+
+    return 0;
 }
