6d08bd4996e4a53cda4624f84a30e234d4fda25b
Script <bigalex934@gmail.com>
2024-04-17 21:38:43 +0300

    Remove unusued imports

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,4 @@
-#include "message.h"
 #include "smtp_client_application.h"
-#include <format>
-#include <fstream>
-#include <iostream>
-#include <stdexcept>
 
 int32_t main(int32_t argc, char *argv[]) {
 
