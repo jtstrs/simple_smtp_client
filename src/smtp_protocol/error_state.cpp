8a812fbc43a0743fa00ef2b442cfdbb060cc3ef8
Script <bigalex934@gmail.com>
2024-05-04 12:22:12 +0300

    Add logging for error state

--- src/smtp_protocol/error_state.cpp
+++ src/smtp_protocol/error_state.cpp
@@ -3,6 +3,7 @@
 
 std::unique_ptr<SmtpState> ErrorState::handleTransition(Poco::Net::StreamSocket &socket,
                                                         const Message &messageData) {
+    m_logger.information(m_errorReason);
     throw std::runtime_error(m_errorReason);
     return nullptr;
 }
--- src/smtp_protocol/error_state.h
+++ src/smtp_protocol/error_state.h
@@ -2,15 +2,20 @@
 #define ERROR_STATE
 
 #include "smtp_state.h"
+#include <Poco/Logger.h>
+
+#define LOG_MODULE "ERROR STATE"
 
 class ErrorState : public SmtpState {
 public:
     template<typename T>
-    explicit ErrorState(T &&errorReason) : m_errorReason(std::move(errorReason)) {}
+    explicit ErrorState(T &&errorReason) : m_errorReason(std::move(errorReason)),
+                                           m_logger(Poco::Logger::get(LOG_MODULE)) {}
     std::unique_ptr<SmtpState> handleTransition(Poco::Net::StreamSocket &socket,
                                                 const Message &messageData) override;
 
 private:
+    Poco::Logger &m_logger;
     std::string m_errorReason;
 };
 
