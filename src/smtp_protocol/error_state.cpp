f2b28784e56e124e69e34c68d698932bfcf77fbe
Script <bigalex934@gmail.com>
2024-05-04 15:01:07 +0300

    Make constructor of ErrorState ordinary function

--- src/smtp_protocol/error_state.cpp
+++ src/smtp_protocol/error_state.cpp
@@ -1,6 +1,12 @@
 #include "error_state.h"
 #include <stdexcept>
 
+#define LOG_MODULE "ERROR STATE"
+
+ErrorState::ErrorState(const std::string &errorReason) : m_errorReason(errorReason),
+                                                         m_logger(Poco::Logger::get(LOG_MODULE)) {
+}
+
 std::unique_ptr<SmtpState> ErrorState::handleTransition(Poco::Net::StreamSocket &socket,
                                                         const Message &messageData) {
     m_logger.information(m_errorReason);
--- src/smtp_protocol/error_state.h
+++ src/smtp_protocol/error_state.h
@@ -4,13 +4,9 @@
 #include "smtp_state.h"
 #include <Poco/Logger.h>
 
-#define LOG_MODULE "ERROR STATE"
-
 class ErrorState : public SmtpState {
 public:
-    template<typename T>
-    explicit ErrorState(T &&errorReason) : m_errorReason(std::move(errorReason)),
-                                           m_logger(Poco::Logger::get(LOG_MODULE)) {}
+    explicit ErrorState(const std::string &errorReason);
     std::unique_ptr<SmtpState> handleTransition(Poco::Net::StreamSocket &socket,
                                                 const Message &messageData) override;
 
