0a54c281ed90b995956fc813e9ee15461ca24335
Script <bigalex934@gmail.com>
2024-05-06 23:09:52 +0300

    Implement fill data state

--- src/smtp_protocol/fill_data_state.cpp
+++ src/smtp_protocol/fill_data_state.cpp
@@ -1,6 +1,35 @@
 #include "fill_data_state.h"
+#include "../logger_wrapper.h"
+#include "error_state.h"
+
+#define LOG_MODULE "FILL DATA STATE"
 
 std::unique_ptr<SmtpState> FillDataState::handleTransition(Poco::Net::StreamSocket &socket,
                                                            const Message &messageData) {
+    LOG_FUNC
+
+    std::stringstream fillDataMessageBuilder;
+    fillDataMessageBuilder << "Subject: " << messageData.subject
+                           << "\r\n";
+
+    std::stringstream mailBodyStream(messageData.body);
+
+    while (!mailBodyStream.eof()) {
+        std::string temp;
+        std::getline(mailBodyStream, temp);
+        fillDataMessageBuilder << std::move(temp) << "\r\n";
+    }
+
+    constexpr char const *SMTP_END_SEQUENCE = "\r\n.";
+    fillDataMessageBuilder << SMTP_END_SEQUENCE;
+
+    const auto status = baseStateHandler(socket,
+                                         fillDataMessageBuilder.str(),
+                                         ResponseCode::ActionCompleted);
+
+    if (status != StateHandlingStatus::Ok) {
+        return std::make_unique<ErrorState>(status);
+    }
+
     return nullptr;
 }
